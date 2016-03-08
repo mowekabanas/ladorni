8436384bfde59c9e6c23bd76f28f5fa5fb5c48b3
dudusbarros <joseeduardobarros@gmail.com>
2016-03-07 21:15:43 -0300

    Add gulp server

---
+++ GulpFile.js
@@ -0,0 +1,59 @@
+var gulp = require('gulp'),
+	concat = require('gulp-concat'),
+	concatCss = require('gulp-concat-css'),
+	jshint = require('gulp-jshint'),
+	minifycss = require('gulp-minify-css'),
+	rename = require('gulp-rename'),
+	uglify = require('gulp-uglify'),
+	imageResize = require('gulp-image-resize'),
+	tinypng = require('gulp-tinypng');
+
+/*
+ * To use the gulp-image-resize, it needs of some dependencies:
+ * https://www.npmjs.com/package/gulp-image-resize
+ *
+ * Or, install:
+ *
+ * Ubuntu:
+ * apt-get install imagemagick
+ * apt-get install graphicsmagick
+ *
+ * Mac:
+ * brew install imagemagick
+ * brew install graphicsmagick
+ *
+ * Windows & others:
+ * http://www.imagemagick.org/script/binary-releases.php
+ * */
+
+var dist = {
+	location: 'dist/'
+};
+
+var images = {
+	content: '*',
+	location: 'img/'
+};
+
+images.largePhotos = {
+	content: '*',
+	location: images.location + 'largePhotos/'
+};
+
+gulp.task('resize', function () {
+	console.log(dist.location + images.largePhotos.location);
+	gulp.src(images.largePhotos.location + images.largePhotos.content)
+		.pipe(imageResize({
+			height : 1080,
+			upscale : false
+		}))
+		.pipe(gulp.dest(dist.location + images.largePhotos.location));
+});
+
+gulp.task('tinyPagePhotos', function () {
+	gulp.src(dist.location + images.largePhotos.location + images.largePhotos.content)
+		.pipe(tinypng('8eNoFlUv4wHzam_8GleKHdhH2YFk9xAd'))
+		.pipe(gulp.dest(dist.location + images.largePhotos.location));
+});
+
+gulp.task('default', ['resize']);
