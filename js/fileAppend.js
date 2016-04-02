d7073eaa518b2815875f5adbdbb59173ecbb35b9
dudusbarros <joseeduardobarros@gmail.com>
2016-04-01 22:03:56 -0300

    Create the FileAppend.js (a fork of Logo.js)

---
+++ js/fileAppend.js
@@ -0,0 +1,79 @@
+
+/* File Append 1.0 */
+
+var FileAppend = (function () {
+
+	/**
+	 * File Append request
+	 * @param viewport {Element}
+	 * @param url {string}
+	 * @param fallback {object}
+	 * @constructor
+	 */
+	function FileAppend(viewport, url, fallback) {
+
+		var self = this;
+
+		this.viewport = viewport;
+		this.url = url;
+		this.fallback = fallback;
+
+		this.get();
+
+	}
+
+	/**
+	 * Append to element
+	 * @param toElement {Element}
+	 * @param before {Element}
+	 */
+	FileAppend.prototype.appendTo = function (toElement, before) {
+
+		if (!before)
+			toElement.appendChild(this.viewport);
+		else
+			toElement.insertBefore(this.viewport, before);
+
+	};
+
+	/**
+	 * Clone the logo and append to element
+	 * @param toElement {Element}
+	 */
+	FileAppend.prototype.cloneTo = function (toElement) {
+
+		toElement.appendChild(this.viewport.cloneNode(this.viewport));
+
+	};
+
+	FileAppend.prototype.get = function () {
+
+		var self = this;
+
+		if (this.viewport && this.url) {
+
+			var request = new XMLHttpRequest();
+			request.open('GET', this.url, true);
+
+			request.onreadystatechange = function() {
+
+				if (this.readyState === 4)
+					if (this.status == 200)
+						if (this.responseText) {
+							self.viewport.innerHTML = this.responseText;
+							if (self.fallback)
+								self.fallback();
+						}
+
+			};
+
+			request.send();
+			request = null;
+
+		}
+
+	};
+
+	return FileAppend;
+
+})();
