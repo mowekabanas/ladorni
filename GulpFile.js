9e94beb716c7dce2933df91f1c29cf181849d8c1
Eduardo Barros <joseeduardobarros@gmail.com>
2020-06-03 07:39:53 -0300

    prettify file

--- GulpFile.js
+++ GulpFile.js
@@ -1,14 +1,14 @@
-var gulp = require('gulp'),
-	concat = require('gulp-concat'),
-	cleanCss = require('gulp-clean-css'),
-	jshint = require('gulp-jshint'),
-	minifycss = require('gulp-minify-css'),
-	rename = require('gulp-rename'),
-	uglify = require('gulp-uglify'),
-	imageResize = require('gulp-image-resize'),
-	tinypng = require('gulp-tinypng'),
-	browserSync = require('browser-sync').create(),
-	reload = browserSync.reload;
+var gulp = require("gulp"),
+  concat = require("gulp-concat"),
+  cleanCss = require("gulp-clean-css"),
+  jshint = require("gulp-jshint"),
+  minifycss = require("gulp-minify-css"),
+  rename = require("gulp-rename"),
+  uglify = require("gulp-uglify"),
+  imageResize = require("gulp-image-resize"),
+  tinypng = require("gulp-tinypng"),
+  browserSync = require("browser-sync").create(),
+  reload = browserSync.reload;
 
 /*
  * To use the gulp-image-resize, it needs of some dependencies:
@@ -28,198 +28,221 @@ var gulp = require('gulp'),
  * http://www.imagemagick.org/script/binary-releases.php
  * */
 
-var tinypngToken = 'tpY_SrTWgniTOP1mYIjhGosNcwY7ieG5';
+var tinypngToken = "tpY_SrTWgniTOP1mYIjhGosNcwY7ieG5";
 
 var dist = {
-	location: './public/dist/'
+  location: "./public/dist/",
 };
 
 dist.css = {
-	location: dist.location + 'css/'
+  location: dist.location + "css/",
 };
 
 dist.js = {
-	location: dist.location + 'js/'
+  location: dist.location + "js/",
 };
 
 var css = {
-	content: '*.css',
-	location: 'css/'
+  content: "*.css",
+  location: "css/",
 };
 
 css.menu = {
-	content: css.content,
-	location: css.location + 'menu/'
+  content: css.content,
+  location: css.location + "menu/",
 };
 
 var js = {
-	content: '*.js',
-	location: 'js/'
+  content: "*.js",
+  location: "js/",
 };
 
 js.menu = {
-	content: js.content,
-	location: js.location + 'menu/'
+  content: js.content,
+  location: js.location + "menu/",
 };
 
 var images = {
-	content: '*',
-	location: 'img/'
+  content: "*",
+  location: "img/",
 };
 
 images.src = {
-	content: '*',
-	location: images.location + 'src/'
+  content: "*",
+  location: images.location + "src/",
 };
 
 images.largePhotos = {
-	content: '*',
-	location: images.location + 'largePhotos/'
+  content: "*",
+  location: images.location + "largePhotos/",
 };
 
 images.verticalPhotos = {
-	content: '*',
-	location: images.location + 'verticalPhotos/'
+  content: "*",
+  location: images.location + "verticalPhotos/",
 };
 
 images.headerPhotos = {
-	content: '*',
-	location: images.location + 'headerPhotos/'
+  content: "*",
+  location: images.location + "headerPhotos/",
 };
 
 images.widePhotos = {
-	content: '*',
-	location: images.location + 'widePhotos/'
+  content: "*",
+  location: images.location + "widePhotos/",
 };
 
 images.winePhotos = {
-	content: '*',
-	location: images.location + 'winePhotos/'
+  content: "*",
+  location: images.location + "winePhotos/",
 };
 
 images.thumbs = {
-	content: '*',
-	location: images.location + 'thumbs/'
+  content: "*",
+  location: images.location + "thumbs/",
 };
 
-gulp.task('distImages', function () {
-	gulp.src(images.location + images.content)
-		.pipe(gulp.dest(dist.location + images.location));
+gulp.task("distImages", function () {
+  gulp
+    .src(images.location + images.content)
+    .pipe(gulp.dest(dist.location + images.location));
 });
 
-gulp.task('resizeLargePhotos', function () {
-	gulp.src(images.largePhotos.location + images.largePhotos.content)
-		.pipe(imageResize({
-			height : 768,
-			upscale : false
-		}))
-		.pipe(gulp.dest(dist.location + images.largePhotos.location));
+gulp.task("resizeLargePhotos", function () {
+  gulp
+    .src(images.largePhotos.location + images.largePhotos.content)
+    .pipe(
+      imageResize({
+        height: 768,
+        upscale: false,
+      })
+    )
+    .pipe(gulp.dest(dist.location + images.largePhotos.location));
 });
 
-gulp.task('resizeVerticalPhotos', function () {
-	gulp.src(images.verticalPhotos.location + images.verticalPhotos.content)
-		.pipe(imageResize({
-			height : 960,
-			upscale : false
-		}))
-		.pipe(gulp.dest(dist.location + images.verticalPhotos.location));
+gulp.task("resizeVerticalPhotos", function () {
+  gulp
+    .src(images.verticalPhotos.location + images.verticalPhotos.content)
+    .pipe(
+      imageResize({
+        height: 960,
+        upscale: false,
+      })
+    )
+    .pipe(gulp.dest(dist.location + images.verticalPhotos.location));
 });
 
-gulp.task('resizeHeaderPhotos', function () {
-	gulp.src(images.headerPhotos.location + images.headerPhotos.content)
-		.pipe(imageResize({
-			height : 1080,
-			upscale : false
-		}))
-		.pipe(gulp.dest(dist.location + images.headerPhotos.location));
+gulp.task("resizeHeaderPhotos", function () {
+  gulp
+    .src(images.headerPhotos.location + images.headerPhotos.content)
+    .pipe(
+      imageResize({
+        height: 1080,
+        upscale: false,
+      })
+    )
+    .pipe(gulp.dest(dist.location + images.headerPhotos.location));
 });
 
-gulp.task('resizeWidePhotos', function () {
-	gulp.src(images.widePhotos.location + images.widePhotos.content)
-		.pipe(imageResize({
-			height : 576,
-			upscale : false
-		}))
-		.pipe(gulp.dest(dist.location + images.widePhotos.location));
+gulp.task("resizeWidePhotos", function () {
+  gulp
+    .src(images.widePhotos.location + images.widePhotos.content)
+    .pipe(
+      imageResize({
+        height: 576,
+        upscale: false,
+      })
+    )
+    .pipe(gulp.dest(dist.location + images.widePhotos.location));
 });
 
-gulp.task('resizeWinePhotos', function () {
-	gulp.src(images.winePhotos.location + images.winePhotos.content)
-		.pipe(imageResize({
-			height : 768,
-			upscale : false
-		}))
-		.pipe(gulp.dest(dist.location + images.winePhotos.location));
+gulp.task("resizeWinePhotos", function () {
+  gulp
+    .src(images.winePhotos.location + images.winePhotos.content)
+    .pipe(
+      imageResize({
+        height: 768,
+        upscale: false,
+      })
+    )
+    .pipe(gulp.dest(dist.location + images.winePhotos.location));
 });
 
 /**
  * The original image need to be 1200x600 (1:2)
  */
-gulp.task('resizeThumbs', function () {
-	gulp.src(images.thumbs.location + images.thumbs.content)
-		.pipe(imageResize({
-			height : 630,
-			upscale : false
-		}))
-		.pipe(gulp.dest(dist.location + images.thumbs.location));
+gulp.task("resizeThumbs", function () {
+  gulp
+    .src(images.thumbs.location + images.thumbs.content)
+    .pipe(
+      imageResize({
+        height: 630,
+        upscale: false,
+      })
+    )
+    .pipe(gulp.dest(dist.location + images.thumbs.location));
 });
 
 // tiny the new images at the 'src' folder
-gulp.task('tinySource', function () {
-	gulp.src(images.src.location + images.src.content)
-		.pipe(tinypng(tinypngToken))
-		.pipe(gulp.dest(images.src.location));
+gulp.task("tinySource", function () {
+  gulp
+    .src(images.src.location + images.src.content)
+    .pipe(tinypng(tinypngToken))
+    .pipe(gulp.dest(images.src.location));
 });
 
-gulp.task('css', function() {
-	gulp.src(css.location + css.content)
-		.pipe(concat('ladorni.css'))
-		.pipe(gulp.dest(dist.css.location));
-	gulp.src(dist.css.location + 'ladorni.css')
-		.pipe(minifycss())
-		.pipe(rename({
-			extname: '.min.css'
-		}))
-		.pipe(gulp.dest(dist.css.location));
+gulp.task("css", function () {
+  gulp
+    .src(css.location + css.content)
+    .pipe(concat("ladorni.css"))
+    .pipe(gulp.dest(dist.css.location));
+  gulp
+    .src(dist.css.location + "ladorni.css")
+    .pipe(minifycss())
+    .pipe(
+      rename({
+        extname: ".min.css",
+      })
+    )
+    .pipe(gulp.dest(dist.css.location));
 });
 
-gulp.task('js', function() {
-	gulp.src(js.location + js.content)
-		.pipe(concat('ladorni.js'))
-		.pipe(gulp.dest(dist.js.location));
+gulp.task("js", function () {
+  gulp
+    .src(js.location + js.content)
+    .pipe(concat("ladorni.js"))
+    .pipe(gulp.dest(dist.js.location));
 });
 
-gulp.task('css-watch', ['css'], function () {
-	browserSync.reload();
+gulp.task("css-watch", ["css"], function () {
+  browserSync.reload();
 });
 
-gulp.task('js-watch', ['js'], function () {
-	browserSync.reload();
+gulp.task("js-watch", ["js"], function () {
+  browserSync.reload();
 });
 
 // Watch scss AND html files, doing different things with each.
-gulp.task('serve', function () {
+gulp.task("serve", function () {
+  // Serve files from the root of this project
+  browserSync.init({
+    server: {
+      baseDir: "./public/",
+      index: "index.html",
+      routes: {
+        "/vinhos": "./public/index.html",
+        "/castelo": "./public/index.html",
+        "/vinicola": "./public/index.html",
+        "/contato": "./public/index.html",
+        "/como-chegar": "./public/index.html",
+      },
+    },
+  });
 
-	// Serve files from the root of this project
-	browserSync.init({
-		server: {
-			baseDir: "./public/",
-			index: "index.html",
-			routes: {
-				"/vinhos": "./public/index.html",
-				"/castelo": "./public/index.html",
-				"/vinicola": "./public/index.html",
-				"/contato": "./public/index.html",
-				"/como-chegar": "./public/index.html"
-			}
-		}
-	});
-
-	gulp.watch([css.location + css.content], ['css-watch']);
-	gulp.watch([js.location + js.content], ['js-watch']);
-	gulp.watch("*.html").on("change", reload);
-	gulp.watch("css/winery/*.css").on("change", reload);
-
+  gulp.watch([css.location + css.content], ["css-watch"]);
+  gulp.watch([js.location + js.content], ["js-watch"]);
+  gulp.watch("*.html").on("change", reload);
+  gulp.watch("css/winery/*.css").on("change", reload);
 });
 
-gulp.task('default', ['serve']);
+gulp.task("default", ["serve"]);
